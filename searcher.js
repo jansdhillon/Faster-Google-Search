77d7e0fec79f7152b2c8ab2fe3d108f9db9a10c7
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2023-03-21 22:56:36 -0700

    fixed name

--- searcher.js
+++ searcher.js
@@ -1,12 +1,11 @@
 document.addEventListener('mousedown', (event) => {
-    // Check if the Option (or Alt) key is pressed
+
+  // If user has pressed Alt or Option key
     if (event.altKey) {
+
+      //Get text, remove whitespaces, search for text in new tab
       const selectedText = window.getSelection().toString().trim();
-  
-      if (selectedText) {
-        const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(selectedText)}`;
-        window.open(googleSearchUrl, '_blank');
-      }
+      window.open(`https://www.google.com/search?q=${encodeURIComponent(selectedText)}`, '_blank');
     }
   });
   
