896f96feced70dc82fcae5ecb9654ffa120b87de
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2023-05-11 22:21:08 -0700

    made version for bing

--- searcher.js
+++ searcher.js
@@ -2,10 +2,9 @@ document.addEventListener('mousedown', (event) => {
 
   // If user has pressed Alt or Option key
     if (event.altKey) {
-
       //Get text, remove whitespaces, search for text in new tab
       const selectedText = window.getSelection().toString().trim();
-      window.open(`https://www.google.com/search?q=${encodeURIComponent(selectedText)}`, '_blank');
+      window.open(`https://www.bing.com/search?q=${encodeURIComponent(selectedText)}`, '_blank');
     }
   });
   
